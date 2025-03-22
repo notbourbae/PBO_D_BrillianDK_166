129852cb4fd99951714a2cd7e3b583f883ef6cdd
notbourbae <desipoetibril@gmail.com>
2025-03-22 13:37:36 +0700

    1

---
+++ TUGASMODUL3/CODELAB1/untitled/src/Main.java
@@ -0,0 +1,69 @@
+
+class KarakterGame {
+    private String nama;
+    private int kesehatan;
+
+    public KarakterGame(String nama, int kesehatan) {
+        this.nama = nama;
+        this.kesehatan = kesehatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public void serang(KarakterGame target) {
+
+    }
+}
+
+class Pahlawan extends KarakterGame {
+    public Pahlawan(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    public void serang(KarakterGame target) {
+        System.out.println(this.getNama() + " menyerang " + target.getNama() + " menggunakan Orbitai Strike!");
+        target.setKesehatan(target.getKesehatan() - 20);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
+
+class Musuh extends KarakterGame {
+    public Musuh(String nama, int kesehatan) {
+        super(nama, kesehatan);
+    }
+
+    public void serang(KarakterGame target) {
+        System.out.println(this.getNama() + " menyerang " + target.getNama() + " menggunakan Snake Bite!");
+        target.setKesehatan(target.getKesehatan() - 15);
+        System.out.println(target.getNama() + " sekarang memiliki kesehatan " + target.getKesehatan());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        KarakterGame karakterUmum = new KarakterGame("Karakter Umum", 100);
+        Pahlawan brimstone = new Pahlawan("Brimstone", 150);
+        Musuh viper = new Musuh("Viper", 200);
+
+        System.out.println("Status awal:");
+        System.out.println(brimstone.getNama() + " memiliki kesehatan: " + brimstone.getKesehatan());
+        System.out.println(viper.getNama() + " memiliki kesehatan: " + viper.getKesehatan());
+
+        brimstone.serang(viper);
+        viper.serang(brimstone);
+    }
+}
